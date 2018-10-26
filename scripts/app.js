5410e923dfe58fb4696bea9a0f76726e317c609b
WeiboFans <KodiAddonUser@gmail.com>
2018-10-26 16:22:14 -0400

    add location request

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,29 @@
 
+'use strict';
 (function() {
   document.getElementById("helloMessage").
     innerHTML = "Hello, world!";
+
+  navigator.permissions.query({name:'geolocation'}).then(function(result) {
+  if (result.state == 'granted') {
+    document.getElementById("helloMessage").
+    innerHTML = "granted, world!";
+  } else if (result.state == 'prompt') {
+    document.getElementById("helloMessage").
+    innerHTML = "prompt, world!";
+  } else {
+    document.getElementById("helloMessage").
+    innerHTML = "Denied, world!";
+  }
+
+  function showMap(position) {
+    // Show a map centered at (position.coords.latitude, position.coords.longitude).
+    document.getElementById("helloMessage").
+    innerHTML = position.coords.latitude + ":" + position.coords.longitude;
+  }
+
+  navigator.geolocation.getCurrentPosition(showMap);
+  });
 
 
   // TODO add service worker code here
@@ -10,4 +32,4 @@
              .register('./service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
-})();
+}());
