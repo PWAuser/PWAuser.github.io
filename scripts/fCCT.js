3be5dc2a7aceb4a738078368f32f9e50efb527f7
WeiboFans <KodiAddonUser@gmail.com>
2018-11-15 13:01:44 -0500

    test navigation demo

--- scripts/fCCT.js
+++ scripts/fCCT.js
@@ -1,12 +1,13 @@
 'use strict';
 (function() {
     document.getElementById("helloMessage").
-    innerHTML = "Welcome to the Demo page.";
+    innerHTML = "Welcome to the out of scope navigation Demo page.";
     
-    var start_btn = $('#btn_start_demo');
-    start_btn.click(function(e) {
+    //var start_btn = $('#btn_start_demo');
+    var start_btn = document.getElementById("btn_start_demo");
+    start_btn.addEventListener('click', function(e) {
       console.log('start_btn clicked');
-      window.scrollTo(0,1);
-      //document.body.requestFullscreen(['hide']);
-    });
+      //window.scrollTo(0,1); this doesn't work
+      document.body.requestFullscreen();
+    }, false);
 }());
