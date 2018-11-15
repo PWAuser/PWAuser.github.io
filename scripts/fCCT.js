beae226fd3e70b589358db9fa7a8e56a28ccd9af
WeiboFans <KodiAddonUser@gmail.com>
2018-11-15 13:35:27 -0500

    try js fullscreen api

--- scripts/fCCT.js
+++ scripts/fCCT.js
@@ -8,6 +8,23 @@
     start_btn.addEventListener('click', function(e) {
       console.log('start_btn clicked');
       //window.scrollTo(0,1); this doesn't work
-      document.body.requestFullscreen();
+      //document.body.requestFullscreen();
+      toggleFullScreen();
     }, false);
+
+    function toggleFullScreen() {
+      var doc = window.document;
+      var docEl = doc.documentElement;
+
+      var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+      var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+      if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+      }
+      else {
+        cancelFullScreen.call(doc);
+      }
+    }
+
 }());
